7d4d20e7b9d33c794c22c067c8d2a0cad4af1b8a
DinisG <dinismogodinho@gmail.com>
2023-03-10 13:56:05 +0000

    Add project files.

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "http://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "115ced9c3e746929ea6645c163623d9f";
+    }
+}
