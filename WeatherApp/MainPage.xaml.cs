433e0bf1fc5e365fead9c3c79e96b92df31f1ac4
DinisG <dinismogodinho@gmail.com>
2023-03-11 16:19:08 +0000

    UI and API connection

--- WeatherApp/MainPage.xaml.cs
+++ WeatherApp/MainPage.xaml.cs
@@ -2,16 +2,30 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	RestService _restService;
 
 	public MainPage()
 	{
 		InitializeComponent();
+		_restService = new RestService();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	async void OnGetWeatherButtonClicked(object sender, EventArgs e)
 	{
-		
+		if (!string.IsNullOrWhiteSpace(_cityEntry.Text))
+		{
+			WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestURL(Constants.OpenWeatherMapEndpoint));
+			BindingContext = weatherData;
+		}
+	}
+
+	string GenerateRequestURL(string endPoint)
+	{
+		string requestUri = endPoint;
+		requestUri += $"?q={_cityEntry.Text}";
+		requestUri += $"&units=metric";
+		requestUri += $"&appid={Constants.OpenWeatherMapAPIKey}";
+		return requestUri;
 	}
 }
 
